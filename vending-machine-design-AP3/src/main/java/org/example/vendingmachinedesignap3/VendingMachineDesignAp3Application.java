823b842570c955c3c9d154fac0dc78559df22190
viraj.pansuriya <viraj.pansuriya@verse.in>
2025-06-22 12:39:22 +0530

    added vending machine flow

---
+++ vending-machine-design-AP3/src/main/java/org/example/vendingmachinedesignap3/VendingMachineDesignAp3Application.java
@@ -0,0 +1,13 @@
+package org.example.vendingmachinedesignap3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMachineDesignAp3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendingMachineDesignAp3Application.class, args);
+    }
+
+}
